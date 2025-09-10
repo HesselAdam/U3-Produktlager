62697d2132aee43d39fb3b3bbe5ce31bc9495f81
HesselAdam <adam.hessel@elev.ga.dbgy.se>
2025-09-10 10:00:08 +0200

    Så nu är jag klar med todo 2. "# Visa produkterna i en numrerad lista"

--- open-test.py
+++ open-test.py
@@ -33,7 +33,15 @@ def load_data(filename):
    
 
 
-#TODO: gör så man kan se en numrerad lista som börjar på 1.
+#TODO: hur gör man så funktionen load_data returnerar products istället?
+def display_products_numbered(products_list):
+    print("\nPRODUKTLISTA")
+    for i, product in enumerate(products_list, 1):
+        print(f"{i}. {product['name']} - {(product['price'])}")
+        print(f"   Beskrivning: {product['desc']}")
+        print(f"   Antal i lager: {product['quantity']}")
+        print()
+
 #TODO: skriv en funktion som returnerar en specifik produkt med hjälp av id
 #TODO: skriv en funktion som tar bort en specifik produkt med hjälp av id
 
@@ -43,6 +51,9 @@ locale.setlocale(locale.LC_ALL, 'sv_SE.UTF-8')
 products = load_data('db_products.csv')
 
 os.system('cls')
+
+# Visa produkterna i en numrerad lista
+display_products_numbered(products)
     
 
 
